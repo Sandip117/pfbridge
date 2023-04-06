782f20a61df51291927a894c0485fb4f656f9094
Rudolph Pienaar <rudolph.pienaar@gmail.com>
2023-04-06 16:40:06 -0400

    Add routes for GET/PUT of pflink URLs

--- pfbridge/routes/relayRouter.py
+++ pfbridge/routes/relayRouter.py
@@ -11,15 +11,105 @@ str_description = """
 
 
 from    fastapi             import  APIRouter, Query, HTTPException, BackgroundTasks, Request
-from    typing              import  List, Dict, Any
+from    typing              import  List, Dict, Any, Union
 
 from    models              import  relayModel
 from    controllers         import  relayController
+
+from    config              import  settings
 
 import  pudb
 
 router          = APIRouter()
 router.tags     = ['Relay services']
+
+@router.put(
+    '/pflink/testURL/',
+    response_model  = relayModel.pflinkURLs,
+    summary         = '''
+    PUT a new value for the pflink testing URL endpoint.
+    '''
+)
+def testURL_update(URL:str) -> relayModel.pflinkURLs:
+    """
+    Description
+    -----------
+
+    Update the internal *testing* URL endpoint of the `pflink` controller.
+    Note that any updates PUT here will *NOT* persist across restarts
+    of `pfbridge` -- on restart these will revert to startup/environement
+    settings.
+
+    Args:
+    -----
+    * `URL` (str): A URL.
+
+    Returns:
+    --------
+    * `relayModel.pflinkURLs`: the updated set of pflinks
+    """
+    settings.pflink.testURL         = URL
+    update:relayModel.pflinkURLs    = relayModel.pflinkURLs()
+    update.productionURL            = settings.pflink.prodURL
+    update.testingURL               = settings.pflink.testURL
+    return update
+
+@router.put(
+    '/pflink/prodURL/',
+    response_model  = relayModel.pflinkURLs,
+    summary         = '''
+    PUT a new value for the pflink testing URL endpoint.
+    '''
+)
+def prodURL_update(URL:str) -> relayModel.pflinkURLs:
+    """
+    Description
+    -----------
+
+    Update the internal *production* URL endpoint of the `pflink` controller.
+    Note that any updates PUT here will *NOT* persist across restarts
+    of `pfbridge` -- on restart these will revert to startup/environement
+    settings.
+
+    Args:
+    -----
+    * `URL` (str): A URL.
+
+    Returns:
+    --------
+    * `relayModel.pflinkURLs`: the updated set of pflinks
+    """
+    settings.pflink.prodURL         = URL
+    update:relayModel.pflinkURLs    = relayModel.pflinkURLs()
+    update.productionURL            = settings.pflink.prodURL
+    update.testingURL               = settings.pflink.testURL
+    return update
+
+@router.get(
+    '/pflink/',
+    response_model  = relayModel.pflinkURLs,
+    summary         = '''
+    GET the internal pflink URLs.
+    '''
+)
+def urls_retFromModel() -> relayModel.pflinkURLs:
+    """
+    Description
+    -----------
+
+    Simply return the URLs to the `pfbridge` with which this
+    `pflink` will communicate. These URLs are typically defined
+    in the environment at `pfbridge`, but can also be set with
+    an appropriate PUT. Note that runtime changes to these URLs
+    are _NOT_ stateful!
+
+    Returns:
+        relayModel.pflinkURLs: The model URLs
+    """
+    current:relayModel.pflinkURLs   = relayModel.pflinkURLs()
+    current.productionURL           = settings.pflink.prodURL
+    current.testingURL              = settings.pflink.testURL
+    return current
 
 @router.post(
     '/analyze/',
@@ -31,7 +121,8 @@ router.tags     = ['Relay services']
 )
 async def workflow_do(
     relayPayload    : relayModel.clientPayload,
-    request         : Request
+    request         : Request,
+    test:bool       = False
 ) -> relayModel.clientResponseSchema:
     """
     Description
@@ -41,6 +132,9 @@ async def workflow_do(
     data set and immediately returns a status update to the
     caller. Multiple calls with the same payload return current
     status of the workflow (or its completion).
+
+    Use the 'test=True' boolean to use the `pflink` test API
+    that simply returns the progressive stages for a workflow.
 
     ```
     class clientPayload(BaseModel):
@@ -81,7 +175,7 @@ async def workflow_do(
     """
     # pudb.set_trace()
     d_ret:relayModel.clientResponseSchema = await relayController.relayAndEchoBack(
-            relayPayload, request
+            relayPayload, request, test
     )
     return d_ret
 
